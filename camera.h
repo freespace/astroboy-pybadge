3c99f2b85afc84a225fc7211dc2cea7ee6c0b2cf
Shuning Bian <freespace@gmail.com>
2022-01-03 16:44:21 +1100

    Adds camera_start_exposure() and camera_start_exposure() functions to handle mirror lockup and...
    data write time delays in a single place.

--- camera.h
+++ camera.h
@@ -6,4 +6,15 @@ void camera_press();
 void camera_release();
 void camera_press_release();
 
+/**
+ * Start an exposure, taking into account camera_mirror_lockup_time_s. Returns
+ * once exposure has started.
+ */ 
+void camera_start_exposure(bool verbose); 
+
+/**
+ * Ends an exposures, takes into account camera_write_time_s. Returns once
+ * data has been stored.
+ */ 
+void camera_end_exposure(bool verbose);
 #endif
--- camera.ino
+++ camera.ino
@@ -20,3 +20,39 @@ void camera_release() {
   digitalWrite(CAMERA_TRIGGER_PIN, LOW);
 }
 
+void camera_start_exposure(bool verbose) {
+    if (SharedConfig.camera_mirror_lockup_time_s > 0) {
+      if (verbose) {
+        arcada.display->println("Engaging Mirror Lockup");
+      }
+
+      // mirror lockup is enabled, so we press-release to lock the mirror
+      camera_press_release();
+
+      // then wait for the configured amount of time for vibrations due to
+      // mirror-slap to go away. This also lets mount vibrations to die away
+      delay(SharedConfig.camera_mirror_lockup_time_s * 1000);
+    }
+
+    // start the capture, assuming camera is in bulb mode
+    camera_press();
+
+    // this is a hack to take into account the delay b/t btn press and exposure
+    // starting on my camera. This could be a setting variable but it probably
+    // doesn't matter given we normally do exposures of >10 seconds 100ms here
+    // and there won't hurt
+    delay(110);
+}
+
+void camera_end_exposure(bool verbose) {
+  // stop exposure assuming camera in bulb mode
+  camera_release();
+
+  // wait for the camera to write out
+  if (verbose) {
+      arcada.display->println("Writing data");
+  }
+
+  delay(SharedConfig.camera_write_time_s * 1000);
+}
+
